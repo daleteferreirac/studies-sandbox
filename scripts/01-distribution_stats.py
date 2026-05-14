e1ed54e874c78c3a54cb494a0d301f9a24bdfe64
Dalete Ferreira <daleteferreirac@gmail.com>
2026-05-13 23:52:24 -0300

    doc

--- scripts/01-distribution_stats.py
+++ scripts/01-distribution_stats.py
@@ -4,17 +4,21 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv("/Users/daleteferreira/PycharmProjects/studies-sandbox/data/WineQT.csv") # turn into dataframe
 
-print(df.head()) # shows 5 rows pattern
-print(df.columns)
+print(df.head()) # shows 5 rows (pattern)
+print(df.columns) # shows index of columns
 
+# select data, starting with one: alcohol
 data = df["alcohol"] # Name: alcohol, Length: 1143, dtype: float64
+
 print("mean: ", np.mean(data))
 print("median: ", np.median(data))
 print("standard deviation: ", np.std(data))
 
-plt.hist(data, bins=20)
+# histogram
+plt.hist(data, bins=20) # bins = groups histogram
 plt.title("Alcohol Distribution")
 plt.xlabel("Alcohol")
 plt.ylabel("Frequency")
 
-plt.show()
+
+
