d2ec568c9455e3e27e7f9f4a3afdcfaf95439bae
Dalete Ferreira <daleteferreirac@gmail.com>
2026-05-22 19:26:52 -0300

    Add comparative boxplots for two wine variables

--- scripts/01-distribution_stats.py
+++ scripts/01-distribution_stats.py
@@ -9,6 +9,8 @@ print(df.columns) # shows index of columns
 
 # select data, starting with one: alcohol
 data = df["alcohol"] # Name: alcohol, Length: 1143, dtype: float64
+acidity = df["fixed acidity"]
+
 
 print("mean: ", np.mean(data))
 print("median: ", np.median(data))
@@ -20,10 +22,18 @@ plt.hist(data, bins=20) # bins = groups histogram
 plt.title("Alcohol Distribution")
 plt.xlabel("Alcohol")
 plt.ylabel("Frequency")
-plt.show()
+#plt.show()
 
 # box plot
 plt.figure()
 plt.boxplot(data)
 plt.title("alcohol distribution")
+#plt.show()
+
+plt.figure()
+plt.boxplot([data, acidity])
+plt.xticks([1, 2], ["alcohol", "acidity"]) # replace numeric x-axis positions with variable names
+plt.title("Variable comparison")
+plt.ylabel("values")
 plt.show()
+
